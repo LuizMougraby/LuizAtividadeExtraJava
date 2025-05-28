02890d885d45e2f2b84e301f657d06f6d3e412ce
muvay <muvay@192.168.0.21>
2025-05-28 15:39:45 -0400

    exibe estado inicial, chama o metodo acelerar e imprime novo estado

--- exercicio_extra_1/src/bike/Main.java
+++ exercicio_extra_1/src/bike/Main.java
@@ -3,6 +3,13 @@ package bike;
 public class Main {
 	public static void main(String[] args) {
         Bicicleta minhaBicicleta = new Bicicleta();
+        
+    System.out.println("Estado inicial:");
+    minhaBicicleta.imprimirEstados();
+
+    System.out.println("Acelerando...");
+    minhaBicicleta.acelerar();
+    minhaBicicleta.imprimirEstados();
 
 
 }
