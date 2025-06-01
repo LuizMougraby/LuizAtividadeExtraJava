377de4756f1ef5d3815b965a49d7359342a74088
muvay <muvay@192.168.0.21>
2025-06-01 10:22:46 -0400

    implementacao do metodo alimentar na classe Leao

--- exercicio_extra_3/src/zoo/Leao.java
+++ exercicio_extra_3/src/zoo/Leao.java
@@ -3,8 +3,13 @@ package zoo;
 public class Leao extends Animal {
 	 @Override
 	 public void emitirSom() {
-	    System.out.println("O leão ruge.");
+	   System.out.println("O leão ruge.");
 	 }
+	 
+	  @Override
+	  public void alimentar() {
+	    System.out.println("O leão está comendo carne.");
+	  }
 
 
 }
