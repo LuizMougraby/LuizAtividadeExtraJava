763ef269f81c64d8087ee1b88b055b02d777f082
muvay <muvay@192.168.0.21>
2025-06-02 14:05:19 -0400

    implementacao do metodo abastecer() em Moto

--- exercicio__extra_4/src/locadora/Moto.java
+++ exercicio__extra_4/src/locadora/Moto.java
@@ -4,5 +4,10 @@ public class Moto extends Veiculo implements Motorizado {
 	public Moto(String modelo, int ano) {
         super(modelo, ano);
     }
+	
+	@Override
+    public void abastecer() {
+        System.out.println("Abastecendo a moto: " + modelo);
+    }
 
 }
