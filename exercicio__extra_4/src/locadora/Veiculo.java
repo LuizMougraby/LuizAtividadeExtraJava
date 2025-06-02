76bef138c5eda4d0574ceab2b751304842341790
muvay <muvay@192.168.0.21>
2025-06-02 13:36:31 -0400

    construtor com modelo e ano

--- exercicio__extra_4/src/locadora/Veiculo.java
+++ exercicio__extra_4/src/locadora/Veiculo.java
@@ -3,5 +3,10 @@ package locadora;
 public abstract class Veiculo {
 	protected String modelo;
     protected int ano;
+    
+    public Veiculo(String modelo, int ano) {
+        this.modelo = modelo;
+        this.ano = ano;
+    }
 
 }
