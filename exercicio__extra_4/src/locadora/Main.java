2833e8cafce627e6c8c635172965b5b862c0b395
muvay <muvay@192.168.0.21>
2025-06-02 14:28:36 -0400

    chama metodo alugar() e abastecer() no main

--- exercicio__extra_4/src/locadora/Main.java
+++ exercicio__extra_4/src/locadora/Main.java
@@ -5,6 +5,14 @@ public class Main {
 		 Carro carro = new Carro("Chevrolet Celta", 2000);
 	     Moto moto = new Moto("Honda CG 160 Titan", 2024);
 	     BicicletaEletrica bike = new BicicletaEletrica("Caloi E-Alta", 2024);
+	     
+	     carro.alugar();
+	     carro.abastecer();
+
+	     moto.alugar();
+	     moto.abastecer();
+
+	     bike.alugar();
 	 }
 
 }
