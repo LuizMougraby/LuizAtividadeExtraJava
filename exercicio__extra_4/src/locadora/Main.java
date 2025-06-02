79fc889e3abba6708c166e55dd60b14b5c878a5b
muvay <muvay@192.168.0.21>
2025-06-02 14:26:45 -0400

    instancia objeto da classe Carro, Moto e BicicletaEletrica no main

--- exercicio__extra_4/src/locadora/Main.java
+++ exercicio__extra_4/src/locadora/Main.java
@@ -2,5 +2,9 @@ package locadora;
 
 public class Main {
 	 public static void main(String[] args) {
+		 Carro carro = new Carro("Chevrolet Celta", 2000);
+	     Moto moto = new Moto("Honda CG 160 Titan", 2024);
+	     BicicletaEletrica bike = new BicicletaEletrica("Caloi E-Alta", 2024);
+	 }
 
 }
