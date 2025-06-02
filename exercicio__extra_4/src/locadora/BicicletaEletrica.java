99e70a56cd5a368cc24d4b409e1ad9d20f864ae6
muvay <muvay@192.168.0.21>
2025-06-02 14:12:36 -0400

    bicicleta nao implementa motorizado e abastecer

--- exercicio__extra_4/src/locadora/BicicletaEletrica.java
+++ exercicio__extra_4/src/locadora/BicicletaEletrica.java
@@ -4,5 +4,7 @@ public class BicicletaEletrica extends Veiculo {
     public BicicletaEletrica(String modelo, int ano) {
         super(modelo, ano);
     }
+    
+ // Não implementa Motorizado nem abastecer()
 
 }
